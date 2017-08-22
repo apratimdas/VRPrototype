8be986b9a2c9e72deb3731f7de708ff5351ee9a7
Apratim <apratimdas@gmail.com>
2017-08-22 10:13:55 -0700

    Test grip functionality

---
+++ Assets/AirGripController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AirGripController : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool gripButtonPressed = false;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(controller==null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+        gripButtonDown = controller.GetPressDown(gripButton);
+        gripButtonUp = controller.GetPressUp(gripButton);
+        gripButtonPressed = controller.GetPress(gripButton);
+
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonUp = controller.GetPressUp(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+
+        if (gripButtonDown)
+        {
+            Debug.Log("Grip button pressed");
+        }
+        if (gripButtonUp)
+        {
+            Debug.Log("Grip button unpressed");
+        }
+        if (triggerButtonDown)
+        {
+            Debug.Log("Trigger button pressed");
+        }
+        if (triggerButtonUp)
+        {
+            Debug.Log("Trigger button unpressed");
+        }
+
+
+    }
+}
