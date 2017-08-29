8f886480e22dd6918230193830f2a8e600c58297
VictorwlChoi <vncvrVictorChoi@Outlook.com>
2017-08-29 15:23:02 -0700

    Controller collider set and button fit

--- Assets/Scripts_Tim/GripManager.cs
+++ Assets/Scripts_Tim/GripManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GripManager : MonoBehaviour {
 
@@ -22,28 +23,34 @@ public class GripManager : MonoBehaviour {
 
         bool isGripped = left.canGrip || right.canGrip;
 
+        if(ldevice.GetTouch(SteamVR_Controller.ButtonMask.ApplicationMenu) || rdevice.GetTouch(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            SceneManager.LoadScene("TestScene");
+        }
+
+
         if(isGripped)
         {
-            if (left.canGrip && ldevice.GetTouch(SteamVR_Controller.ButtonMask.Grip)) //Can Change control here
+            if (left.canGrip && (ldevice.GetTouch(SteamVR_Controller.ButtonMask.Grip) || ldevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger))) //Can Change control here
             {
                 body.useGravity = false;
-                body.isKinematic = true;
+                body.isKinematic = true; //should be true
                 body.transform.position += (left.previousPos - left.transform.localPosition);
             }
-            else if (left.canGrip && ldevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+            else if (left.canGrip && (ldevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip) || ldevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger)))
             {
                 body.useGravity = true;
-                body.isKinematic = false;
+                body.isKinematic = false; //should be false
                 body.velocity = (left.previousPos - left.transform.localPosition) / Time.deltaTime;
             }
 
-            if (right.canGrip && rdevice.GetTouch(SteamVR_Controller.ButtonMask.Grip)) //Can Change control here
+            if (right.canGrip && (rdevice.GetTouch(SteamVR_Controller.ButtonMask.Grip) || rdevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger))) //Can Change control here
             {
                 body.useGravity = false;
                 body.isKinematic = true;
                 body.transform.position += (right.previousPos - right.transform.localPosition);
             }
-            else if (right.canGrip && rdevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+            else if (right.canGrip && (rdevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip) || rdevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger)))
             {
                 body.useGravity = true;
                 body.isKinematic = false;
