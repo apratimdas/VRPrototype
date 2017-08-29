1c32102f78eb4c568bee14f9fc08ea883b868f0d
Apratim <apratimdas@gmail.com>
2017-08-29 10:22:38 -0700

    Working prototype

---
+++ Assets/Scripts_Tim/GripManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripManager : MonoBehaviour {
+
+    public Rigidbody body;
+
+    public Pull left;
+    public Pull right;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void FixedUpdate()
+    {
+        var ldevice = SteamVR_Controller.Input((int)left.controller.index);
+        var rdevice = SteamVR_Controller.Input((int)right.controller.index);
+
+        bool isGripped = left.canGrip || right.canGrip;
+
+        if(isGripped)
+        {
+            if (left.canGrip && ldevice.GetTouch(SteamVR_Controller.ButtonMask.Grip)) //Can Change control here
+            {
+                body.useGravity = false;
+                body.isKinematic = true;
+                body.transform.position += (left.previousPos - left.transform.localPosition);
+            }
+            else if (left.canGrip && ldevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+            {
+                body.useGravity = true;
+                body.isKinematic = false;
+                body.velocity = (left.previousPos - left.transform.localPosition) / Time.deltaTime;
+            }
+
+            if (right.canGrip && rdevice.GetTouch(SteamVR_Controller.ButtonMask.Grip)) //Can Change control here
+            {
+                body.useGravity = false;
+                body.isKinematic = true;
+                body.transform.position += (right.previousPos - right.transform.localPosition);
+            }
+            else if (right.canGrip && rdevice.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+            {
+                body.useGravity = true;
+                body.isKinematic = false;
+                body.velocity = (right.previousPos - right.transform.localPosition) / Time.deltaTime;
+            }
+        }
+        else
+        {
+            body.useGravity = true;
+            body.isKinematic = false;
+        }
+
+
+        
+
+        left.previousPos = left.transform.localPosition;
+        right.previousPos = right.transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts_Tim/Pull.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pull : MonoBehaviour {
+    
+
+    public SteamVR_TrackedObject controller;
+
+    [HideInInspector]
+    public Vector3 previousPos;
+
+    public bool canGrip;
+
+	// Use this for initialization
+	void Start () {
+        previousPos = controller.transform.localPosition;
+	}
+
+    // Update is called once per frame
+    //void FixedUpdate () {
+    //    var device = SteamVR_Controller.Input((int)controller.index);
+    //    if(canGrip && device.GetTouch(SteamVR_Controller.ButtonMask.Grip)) //Can Change control here
+    //    {
+    //        body.useGravity = false;
+    //        body.isKinematic = true;
+    //        body.transform.position += (previousPos - controller.transform.localPosition);
+    //    }
+    //    else if(canGrip && device.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+    //    {
+    //        body.useGravity = true;
+    //        body.isKinematic = false;
+    //        body.velocity = (previousPos - controller.transform.localPosition) / Time.deltaTime;
+    //    }
+    //    else
+    //    {
+    //        body.useGravity = true;
+    //        body.isKinematic = false;
+    //    }
+
+    //    previousPos = controller.transform.localPosition;
+
+
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canGrip = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canGrip = false;
+    }
+}
