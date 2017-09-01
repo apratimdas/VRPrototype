4d322cc21e7aa7c436be380e8f6367401ba43c0e
VictorwlChoi <vncvrVictorChoi@Outlook.com>
2017-09-01 08:45:50 -0700

    Portal testing

---
+++ Assets/stepThroughPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stepThroughPortal : MonoBehaviour {
+
+    public GameObject otherPortal;
+    public GameObject body;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            Debug.Log("hit!");
+            body.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 3;
+        }
+    }
+}
