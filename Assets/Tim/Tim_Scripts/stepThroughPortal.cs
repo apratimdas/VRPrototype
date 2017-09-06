cfcc6bc9b6652bd774e2707f76d25ea713d23e6b
apratimdas <apratimdas@gmail.com>
2017-09-05 18:09:10 -0700

    Bugfix

--- Assets/Tim/Tim_Scripts/stepThroughPortal.cs
+++ Assets/Tim/Tim_Scripts/stepThroughPortal.cs
@@ -23,7 +23,8 @@ public class stepThroughPortal : MonoBehaviour {
         {
             Debug.Log("hit!");
             body.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 3;
-            body.transform.forward = -otherPortal.transform.forward;
+            Vector3 forward = new Vector3(0, Camera.main.transform.forward.y, 0);
+            body.transform.forward = forward;
             body.GetComponent<Rigidbody>().velocity = body.transform.rotation * body.GetComponent<Rigidbody>().velocity;
         }
     }
